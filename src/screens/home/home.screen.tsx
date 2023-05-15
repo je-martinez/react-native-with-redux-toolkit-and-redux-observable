ed75be635a5da5ae959e2dbb6cc7a227e27115f2
Jose E. Martinez <josemartinezchevez@gmail.com>
2023-05-14 22:00:22 -0600

    feat: working example with redux observable

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -1,28 +1,55 @@
-import React, { useEffect } from 'react';
-import { Button, SafeAreaView, Text } from 'react-native';
+import React from 'react';
+import { Button, FlatList, SafeAreaView, Text } from 'react-native';
 import { useAppDispatch } from 'store';
+import { getPosts } from 'store/epics/posts.epic';
 import { useAppSelector } from 'store/index';
-import { increment } from 'store/slices';
 
 const HomeScreen = (): JSX.Element => {
-  const dispatch = useAppDispatch();
-  const { value } = useAppSelector(store => store.counter);
-
-  useEffect(() => {
-    dispatch(increment());
-  }, [dispatch]);
-
-  const incrementValue = () => {
-    dispatch(increment());
-  };
+  const { posts } = useAppSelector(store => store.posts);
 
   return (
     <SafeAreaView>
-      <Text>Home Screen</Text>
-      <Text>{value}</Text>
-      <Button title="Hola" onPress={() => incrementValue()} />
+      <FlatList
+        ListEmptyComponent={NoPostLoaded}
+        ListHeaderComponent={HeaderFlatlist}
+        data={posts}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
     </SafeAreaView>
   );
+};
+
+const HeaderFlatlist = () => {
+  const dispatch = useAppDispatch();
+
+  const getPostsFromApi = () => {
+    dispatch(getPosts());
+  };
+
+  return (
+    <Button
+      title="Fetch Posts"
+      onPress={() => {
+        getPostsFromApi();
+      }}
+    />
+  );
+};
+
+const NoPostLoaded = (): JSX.Element => {
+  const { loadingFetchPost, errorFetchPost } = useAppSelector(
+    store => store.posts,
+  );
+
+  if (loadingFetchPost) {
+    return <Text>Loading posts...</Text>;
+  }
+
+  if (errorFetchPost) {
+    return <Text>Failed trying to retrieve posts...</Text>;
+  }
+
+  return <Text>No post loaded!</Text>;
 };
 
 export default HomeScreen;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,24 @@
 import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit';
-import { counterSlice } from './slices';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { combineEpics, createEpicMiddleware } from 'redux-observable';
+import { postsEpic } from './epics/posts.epic';
+import { postsSlice } from './slices/posts.slice';
+
+const rootEpic = combineEpics(postsEpic);
+const epicMiddleware = createEpicMiddleware();
 
 const makeStore = () =>
   configureStore({
     reducer: {
-      [counterSlice.name]: counterSlice.reducer,
+      [postsSlice.name]: postsSlice.reducer,
     },
     devTools: true,
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware().concat(epicMiddleware),
   });
 
 export const store = makeStore();
+epicMiddleware.run(rootEpic);
 export type AppStore = ReturnType<typeof makeStore>;
 export type AppState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
